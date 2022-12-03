2a8b95c6e2f3cd7b24263327dd68922da4a55adf
kyou <67418265+kyou-nase@users.noreply.github.com>
2022-12-03 16:51:02 +0800

    Added UI

--- Jiju Airlines Reservation.cpp
+++ Jiju Airlines Reservation.cpp
@@ -1,9 +1,111 @@
 // Jiju Airlines Reservation v.0.0.01
 
-#include <iostream>
+#include <iostream> //standard input/output streams
+#include <iomanip> //setw
+#include <cctype> //declares a set of functions to classify and transform characters
+#include <ctime> //functions to get and use date and time information
+#include <windows.h> //SetConsoleCursorPosition, SetConsoleCursor, gotoxy,GetAsyncKeyState
+#include <sstream> // stringstream
+#include <string>
+#pragma warning(disable: 4996)
+
 using namespace std;
 
-int main()
+void gotoxy(int x, int y) //set cursor position to a exact coordinate
 {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+void Color(int color) //function to change color of text and backround
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+void ShowConsoleCursor(bool showFlag) //function to enable or disable cursor
+{
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO     cursorInfo;
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = showFlag; // set the cursor visibility
+	SetConsoleCursorInfo(out, &cursorInfo);
+}
+const std::string currentTime()
+{
+	time_t now = time(0);
+	struct tm tstruct;
+	char buf[80];
+	tstruct = *localtime(&now);
+	strftime(buf, sizeof(buf), "%H:%M:%S", &tstruct);
+	return buf;
+}
 
+bool alpha(string name) { //function to check a string if it contains characters other than alphanumerics
+	for (int z = 0; z < name.size(); z++) {
+		if (!isalpha(name[z])) {
+			if (isspace(name[z])) {
+				continue;
+			}
+			else
+				return false;
+		}
+
+	}
+	return true;
+}
+
+int main() {
+	string logo = "\t\t\t\t\t  \xc9\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbb\n\t\t\t\t\t  \xba       \xdf\xdf\xdb \xdf\xdb\xdb\xdf  \xdf\xdf\xdb \xdb  \xdb      \xba\n   \t\t\t\t\t  \xba         \xdb  \xde\xdd     \xdb \xdb  \xdb      \xba\n   \t\t\t\t\t  \xba      \xdf\xdc\xdc\xdf \xdc\xdb\xdb\xdc \xdf\xdc\xdc\xdf \xdf\xdc\xdc\xdf      \xba\n \t\t\t\t\t  \xba A    I    R    L    I    N   E\xba\n\t\t\t\t\t  \xba     R E S E R V A T I O N S   \xba\n\t\t\t\t\t  \xc8\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbc";
+	int x; char yn = 'x';
+	bool running = true;
+	time_t t = time(0);
+
+	system("color f1"); //set the main color of text and backround of program
+	cout << logo;
+	cout << "\n\n\n\t\t\t\t" << char(201);      for (int i = 0; i < 51; i++) { cout << char(205); }      cout << char(187) << endl;
+	cout << "\t\t\t\t" << char(186); Color(240); cout << "                                                   "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t" << char(186); Color(240); cout << "                     WELCOME                       "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t" << char(186); Color(240); cout << "                                                   "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t" << char(186); Color(240); cout << "  WOULD YOU LIKE TO MAKE AN AIRLINE RESERVATION?   "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t" << char(186); Color(240); cout << "                  YES       NO                     "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t" << char(186); Color(240); cout << "                                                   "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t" << char(200);      for (int i = 0; i < 51; i++) { cout << char(205); }      cout << char(188) << endl;
+	gotoxy(50, 14);
+	gotoxy(1, 14);
+	ShowConsoleCursor(false); //make cursor invisible
+	cout << "\t\t\t\t"; Color(241); cout << char(186) << "                 " << ">YES"; Color(240); cout << "       NO                     "; Color(241); cout << char(186) << endl;
+	yn = 'Y';
+	gotoxy(54, 14);
+
+	system("pause>nul");
+	while (running) { //run a loop with a bool as a condition
+
+		if (GetAsyncKeyState(VK_RIGHT)) { //detect if left arrow key is pressed
+			gotoxy(1, 14);
+			cout << "\t\t\t\t" << char(186); Color(240); cout << "                  YES"; Color(241); cout << "      >NO" << "                     " << char(186) << endl; //moves arrow and highltght NO
+			yn = 'N';
+			gotoxy(63, 14);
+		}
+		if (GetAsyncKeyState(VK_LEFT)) { //detect if right arrow key is pressed
+			gotoxy(1, 14);
+			cout << "\t\t\t\t"; Color(241); cout << char(186) << "                 " << ">YES"; Color(240); cout << "       NO                     "; Color(241); cout << char(186) << endl; //moves arrow and highltght YES
+			yn = 'Y';
+			gotoxy(54, 14);
+		}
+		if (GetAsyncKeyState(VK_RETURN)) { //detect if enter key is pressed
+			switch (yn) { //switch case of decision of user
+			case 'y': case 'Y':
+				system("cls");
+				running = false; //sets bool value to false to stop loop
+				gotoxy(1, 1);
+				break;
+			case 'n': case 'N': // ends program if NO isthe choice
+				system("cls");
+				cout << logo;
+				cout << "\n\n\n\t\t\t\t\t      Thank You! And Goodbye!";
+				return 0;
+				break;
+			}
+		}
+	}
 }
