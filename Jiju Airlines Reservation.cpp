b4936f8fd5e3768549b69da4ce3a06d93aeb0ed6
kyou <67418265+kyou-nase@users.noreply.github.com>
2022-12-05 18:37:48 +0800

    New Loading Screen and Destination Selection

--- Jiju Airlines Reservation.cpp
+++ Jiju Airlines Reservation.cpp
@@ -54,12 +54,30 @@ bool alpha(string name) { //function to check a string if it contains characters
 	return true;
 }
 
+void loading_screen()
+{
+	cout << "\n\n\n\n\n\t\t\t\t\t\xfe JIJU Airlines Reservations is Loading \xfe\n\n";
+	char a = 177, b = 219;
+	cout << "\t\t\t\t\t\t";
+	for (int i = 0; i <= 22; i++)
+		cout << a;
+	cout << "\r";
+	cout << "\t\t\t\t\t\t";
+	for (int i = 0; i <= 22; i++)
+	{
+		cout << b;
+		for (int j = 0; j <= 3e7; j++);
+	}
+}
+
 int main() {
 	string logo = "\t\t\t\t\t  \xc9\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbb\n\t\t\t\t\t  \xba       \xdf\xdf\xdb \xdf\xdb\xdb\xdf  \xdf\xdf\xdb \xdb  \xdb      \xba\n   \t\t\t\t\t  \xba         \xdb  \xde\xdd     \xdb \xdb  \xdb      \xba\n   \t\t\t\t\t  \xba      \xdf\xdc\xdc\xdf \xdc\xdb\xdb\xdc \xdf\xdc\xdc\xdf \xdf\xdc\xdc\xdf      \xba\n \t\t\t\t\t  \xba A    I    R    L    I    N   E\xba\n\t\t\t\t\t  \xba     R E S E R V A T I O N S   \xba\n\t\t\t\t\t  \xc8\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbc";
 	int x; char yn = 'x';
+	char destination;
 	bool running = true;
 	time_t t = time(0);
 
+	// Menu: Welcome Screen
 	system("color f1"); //set the main color of text and backround of program
 	cout << logo;
 	cout << "\n\n\n\t\t\t\t" << char(201);      for (int i = 0; i < 51; i++) { cout << char(205); }      cout << char(187) << endl;
@@ -77,8 +95,9 @@ int main() {
 	yn = 'Y';
 	gotoxy(54, 14);
 
+	// Input: Y/N
 	system("pause>nul");
-	while (running) { //run a loop with a bool as a condition
+	while (running) { // run a loop with a bool as a condition
 
 		if (GetAsyncKeyState(VK_RIGHT)) { //detect if left arrow key is pressed
 			gotoxy(1, 14);
@@ -108,4 +127,54 @@ int main() {
 			}
 		}
 	}
+
+	// Output: Loading Screen
+	cout << logo;
+	loading_screen();
+
+	// Input: Enter any key to Proceed Screen
+	system("cls");
+	cout << logo;
+	cout << "\n\n\t\t\t ";
+	cout << char(201);      for (int i = 0; i < 66; i++) { cout << char(205); }      cout << char(187) << endl;
+	cout << "\t\t\t " << char(186) << setw(67) << char(186) << endl;
+	cout << "\t\t\t " << char(186) << setw(67) << char(186) << endl;
+	cout << "\t\t\t " << char(186); Color(240); cout << "                     Press Any Key to Proceed  "; Color(241); cout << setw(20) << char(186) << endl;
+	cout << "\t\t\t " << char(186) << setw(67) << char(186) << endl;
+	cout << "\t\t\t " << char(186) << setw(67) << char(186) << endl;
+	cout << "\t\t\t " << char(200);      for (int i = 0; i < 66; i++) { cout << char(205); }      cout << char(188) << endl;
+	system("pause>nul");
+
+	// Menu: Destination Selection
+	system("cls");
+	cout << logo;
+	//prints all of available trip destinations
+	cout << "\n\n\n\n\t\t\t\t      " << char(201);  for (int i = 0; i < 40; i++) { cout << char(205); }  cout << char(187) << endl;
+	cout << "\t\t\t\t      " << char(186) << "            \xfe DESTINATIONS \xfe            " << char(186) << endl;
+	cout << "\t\t\t\t      " << char(204);  for (int i = 0; i < 40; i++) { cout << char(205); }  cout << char(185) << endl;
+	cout << "\t\t\t\t      " << char(186) << "                                        " << char(186) << endl;
+	cout << "\t\t\t\t      " << char(186) << "\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0 DOMESTIC FLIGHTS \xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0" << char(186) << endl;
+	cout << "\t\t\t\t      " << char(186) << "                                        " << char(186) << endl;
+	cout << "\t\t\t\t      " << char(186); Color(240); cout << "             (A) \xb3 Batanes              "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t      " << char(186); Color(240); cout << "             (B) \xb3 Bacolod              "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t      " << char(186); Color(240); cout << "             (C) \xb3 Palawan              "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t      " << char(186); Color(240); cout << "             (D) \xb3 Davao                "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t      " << char(186) << "                                        " << char(186) << endl;
+	cout << "\t\t\t\t      " << char(204);  for (int i = 0; i < 40; i++) { cout << char(205); }  cout << char(185) << endl;
+	cout << "\t\t\t\t      " << char(186); Color(240); cout << "      Please Refer from the Above       "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t      " << char(186); Color(240); cout << "Enter Your Desired Destination(A - D):  "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t      " << char(200);      for (int i = 0; i < 40; i++) { cout << char(205); }      cout << char(188) << endl;
+	gotoxy(77, 23);
+	cin >> destination;
+
+
+
+
+
+
+
+
+
+
+
 }
