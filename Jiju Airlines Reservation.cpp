c3e68a997abbae8b1f3ab86ec94b3431b4f85950
kyou <67418265+kyou-nase@users.noreply.github.com>
2022-12-08 15:16:09 +0800

    Set arrow keys as Function, partially added receipt and input validation still has skillissues

--- Jiju Airlines Reservation.cpp
+++ Jiju Airlines Reservation.cpp
@@ -64,7 +64,7 @@ void ignoreLine() //function to ignore last input of user for cin.fail()
     cin.ignore();
 }
 
-bool input_validation(string input)
+bool inputValidation_passed(string input)
 {
     bool valid_pass = true;
     if ((input[0] == '0' && isdigit(input[1]))) // input can't start from 0
@@ -80,6 +80,49 @@ bool input_validation(string input)
     return valid_pass;
 }
 
+void arrow_keys() {
+	char yn = 'x';
+	string logo = "\t\t\t\t\t  \xc9\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbb\n\t\t\t\t\t  \xba     \xdb   \xdb \xdb\xdf\xdf\xdf \xdb\xdf\xdf\xdc \xdb\xdf\xdf\xdb      \xba\n   \t\t\t\t\t  \xba     \xdb\xdb \xdb\xdb \xdb\xfe\xfe  \xdb\xdc\xdc\xdf \xdb\t  \xba\n   \t\t\t\t\t  \xba     \xdb \xdb \xdb \xdb\xdc\xdc\xdc \xdb  \xdb \xdb\xdc\xdc\xdb      \xba\n \t\t\t\t\t  \xba A    I    R    L    I    N   E\xba\n\t\t\t\t\t  \xba     R E S E R V A T I O N S   \xba\n\t\t\t\t\t  \xc8\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbc";
+	bool running = true;
+
+	gotoxy(50, 14);
+	gotoxy(1, 14);
+	ShowConsoleCursor(false); //make cursor invisible
+	cout << "\t\t\t\t"; Color(241); cout << char(186) << "                 " << ">YES"; Color(240); cout << "       NO                     "; Color(241); cout << char(186) << endl;
+	yn = 'Y';
+	gotoxy(54, 14);
+	system("pause>nul");
+	while (running) { //run a loop with a bool as a condition
+
+		if (GetAsyncKeyState(VK_RIGHT)) { //detect if left arrow key is pressed
+			gotoxy(1, 14);
+			cout << "\t\t\t\t" << char(186); Color(240); cout << "                  YES"; Color(241); cout << "      >NO" << "                     " << char(186) << endl; //moves arrow and highltght NO
+			yn = 'N';
+			gotoxy(63, 14);
+		}
+		if (GetAsyncKeyState(VK_LEFT)) { //detect if right arrow key is pressed
+			gotoxy(1, 14);
+			cout << "\t\t\t\t"; Color(241); cout << char(186) << "                 " << ">YES"; Color(240); cout << "       NO                     "; Color(241); cout << char(186) << endl; //moves arrow and highltght YES
+			yn = 'Y';
+			gotoxy(54, 14);
+		}
+		if (GetAsyncKeyState(VK_RETURN)) { //detect if enter key is pressed
+			switch (yn) { //switch case of decision of user
+			case 'y': case 'Y':
+				system("cls");
+				running = false; //sets bool value to false to stop loop
+				gotoxy(1, 1);
+				break;
+			case 'n': case 'N': // ends program if NO isthe choice
+				system("cls");
+				cout << logo;
+				cout << "\n\n\n\t\t\t\t\t      Thank You! And Goodbye!";
+				exit;
+				break;
+			}
+		}
+	}
+}
 // Loading Screen Layout
 void loading() {
 	cout << "\n\n\n\n\n\t\t\t\t\t\xfe JIJU Airlines Reservations is Loading \xfe\n\n";
@@ -96,6 +139,7 @@ void loading() {
 	}
 }
 
+// Press Any Key to Proceed
 void any_keypress() {
 	cout << char(201);      for (int i = 0; i < 66; i++) { cout << char(205); }      cout << char(187) << endl;
 	cout << "\t\t\t " << char(186) << setw(67) << char(186) << endl;
@@ -106,16 +150,25 @@ void any_keypress() {
 	cout << "\t\t\t " << char(200);      for (int i = 0; i < 66; i++) { cout << char(205); }      cout << char(188) << endl;
 	system("pause>nul");
 	system("cls");
-	
 }
 
 int main() {
-	string date = "", year, receipt_flightType, destination, receipt_destination, logo = "\t\t\t\t\t  \xc9\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbb\n\t\t\t\t\t  \xba       \xdf\xdf\xdb \xdf\xdb\xdb\xdf  \xdf\xdf\xdb \xdb  \xdb      \xba\n   \t\t\t\t\t  \xba         \xdb  \xde\xdd     \xdb \xdb  \xdb      \xba\n   \t\t\t\t\t  \xba      \xdf\xdc\xdc\xdf \xdc\xdb\xdb\xdc \xdf\xdc\xdc\xdf \xdf\xdc\xdc\xdf      \xba\n \t\t\t\t\t  \xba A    I    R    L    I    N   E\xba\n\t\t\t\t\t  \xba     R E S E R V A T I O N S   \xba\n\t\t\t\t\t  \xc8\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbc";
+	string dateInput, receipt_flightType, destination, receipt_destination, logo = "\t\t\t\t\t  \xc9\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbb\n\t\t\t\t\t  \xba       \xdf\xdf\xdb \xdf\xdb\xdb\xdf  \xdf\xdf\xdb \xdb  \xdb      \xba\n   \t\t\t\t\t  \xba         \xdb  \xde\xdd     \xdb \xdb  \xdb      \xba\n   \t\t\t\t\t  \xba      \xdf\xdc\xdc\xdf \xdc\xdb\xdb\xdc \xdf\xdc\xdc\xdf \xdf\xdc\xdc\xdf      \xba\n \t\t\t\t\t  \xba A    I    R    L    I    N   E\xba\n\t\t\t\t\t  \xba     R E S E R V A T I O N S   \xba\n\t\t\t\t\t  \xc8\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbc";
+	struct	//Structure for seperate date inputs
+	{
+		int year, month, day;
+	} date;
+
 	int x, flightType;
 	double ticketPrice;
 	char yn = 'x';
-	bool is_num_bool = 0, running = true;
-	time_t t = time(0);
+	bool running = true;
+
+	stringstream convertString(dateInput);
+
+	time_t timeNow = time(0); 				//get current time from local system
+	tm* localTime = localtime(&timeNow);
+
 
 	// Input: Welcome Screen [Y/N]
 	system("color f1"); //set the main color of text and backround of program
@@ -137,36 +190,7 @@ int main() {
 
 	// Process: Reservation for L/R and Return Input
 	system("pause>nul");
-	while (running) { //run a loop with a bool as a condition
-
-		if (GetAsyncKeyState(VK_RIGHT)) { //detect if left arrow key is pressed
-			gotoxy(1, 14);
-			cout << "\t\t\t\t" << char(186); Color(240); cout << "                  YES"; Color(241); cout << "      \x10NO" << "                     " << char(186) << endl; //moves arrow and highltght NO
-			yn = 'N';
-			gotoxy(63, 14);
-		}
-		if (GetAsyncKeyState(VK_LEFT)) { //detect if right arrow key is pressed
-			gotoxy(1, 14);
-			cout << "\t\t\t\t"; Color(241); cout << char(186) << "                 " << "\x10YES"; Color(240); cout << "       NO                     "; Color(241); cout << char(186) << endl; //moves arrow and highltght YES
-			yn = 'Y';
-			gotoxy(54, 14);
-		}
-		if (GetAsyncKeyState(VK_RETURN)) { //detect if enter key is pressed
-			switch (yn) { //switch case of decision of user
-			case 'y': case 'Y':
-				system("cls");
-				running = false; //sets bool value to false to stop loop
-				gotoxy(1, 1);
-				break;
-			case 'n': case 'N': // ends program if NO isthe choice
-				system("cls");
-				cout << logo;
-				cout << "\n\n\n\t\t\t\t\t      Thank You! And Goodbye!\n\n\n";
-				return 0;
-				break;
-			}
-		}
-	}
+	arrow_keys();
 
 	// Output: Loading Screen
 	cout << logo;
@@ -268,7 +292,7 @@ int main() {
 	// Input: Date Schedule 
 	system("cls");
 	cout << logo;
-	tm* now = localtime(&t); //get current time or date
+	tm* now = localtime(&timeNow); //get current time or date
 	// This will print out and lets the user input their schedule of flight
 	cout << "\n\n\n\t\t      " << char(201);  for (int i = 0; i < 74; i++) { cout << char(205); }  cout << char(187) << endl;
 	cout << "\t\t      " << char(186); Color(240); cout << "       When would you like your reservation flight to be scheduled?       "; Color(241); cout << char(186) << endl;
@@ -284,11 +308,9 @@ int main() {
 	cout << "\t\t      " << char(186) << setw(75) << char(186) << endl;
 	cout << "\t\t      " << char(200);      for (int i = 0; i < 74; i++) { cout << char(205); }      cout << char(188) << endl;
 	gotoxy(58, 15);
-	getline(cin >> ws, date); //input of year of schedule
+	getline(cin >> ws, dateInput); //input of year of schedule
 	//loop to validate whether input string has any other characters other than numeric
-	
-	
-	
+
 	
 
 
@@ -298,58 +320,96 @@ int main() {
 
 
 
+	// IGNORE THIS LINE FIRST(WILL BE USED IN THE LAST SECTION)
 	//pause program so user can review or check the booking summary
-	cout << "\n\n\n\n\t\t\t ";
+	system("cls");
+	//prints out booking summary of reservation
+	cout << logo;
+	cout << "\n\n\n\t\t\t" << char(201);      for (int i = 0; i < 67; i++) { cout << char(205); }      cout << char(187) << endl;
+	cout << "\t\t\t" << char(186) << "                         \xfe BOOKING SUMMARY \xfe                       " << char(186) << endl;
+	cout << "\t\t\t" << char(204);      for (int i = 0; i < 67; i++) { cout << char(205); }      cout << char(185) << endl;
+	cout << "\t\t\t" << char(186) << "\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0 BOOKING DESTINATION \xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0" << char(186) << endl;
+	cout << "\t\t\t" << char(186); Color(240); cout << "  \t\t    Manila --------TO--------    "; Color(241); cout << setw(20) << char(186) << endl;
+	gotoxy(70, 13);
+	cout << receipt_destination;
+	/*
+	*gotoxy(1, 14);
+	cout << "\t\t\t" << char(186) << "\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0\xb0" << char(186) << endl;
+	cout << "\t\t\t" << char(204);      for (int i = 0; i < 67; i++) { cout << char(205); }      cout << char(185) << endl;
+	cout << "\t\t\t" << char(186); Color(240); cout << "  Departure Date: " << "\t\t\t\t   "; Color(241); cout << setw(18) << char(186) << endl;
+	gotoxy(78, 16);
+	cout << day << "/" << month << "/" << year;
+	gotoxy(1, 17);
+	x = 20;
+	if (type[0] == '2') { //display return date if two-way flight
+		cout << "\t\t\t" << char(186) << "   \t\t\t\t\t\t\t\t    " << char(186) << endl;
+		cout << "\t\t\t" << char(186); Color(240); cout << "     Return Date: " << "\t\t\t\t   "; Color(241); cout << setw(18) << char(186) << endl;
+		gotoxy(78, 18);
+		Color(240); cout << day1 << "/" << month1 << "/" << year1; Color(241);
+		gotoxy(1, 19);
+		x += 2;
+	}
+	cout << "\t\t\t" << char(186) << "   \t\t\t\t\t\t\t\t    " << char(186) << endl;
+	cout << "\t\t\t" << char(186) << "                           \xfe PASSENGERS \xfe                          " << char(186) << endl;
+	while (ctr1 != pssngr) { //prints out each passengers details and computation for their corresponding passengers fee
+		cout << "\t\t\t" << char(186) << "   \t\t\t\t\t\t\t\t    " << char(186) << endl;
+		cout << "\t\t\t" << char(186); Color(240); cout << " " << index1 << ". " << surname[index1] << ", " << frstname[index1] << " " << mi[index1]; Color(241);
+		gotoxy(92, x);
+		cout << char(186) << endl;
+		cout << "\t\t\t" << char(186); Color(240); cout << "     Trip Fare"; Color(241); cout << setw(54) << char(186) << endl;
+		x++;
+		
+		gotoxy(79, x);
+		Color(240); cout << "P" << ticketPrice; Color(241); cout << endl;
+		x++;
+		if (young[index3] == 1) { //prints if passenger is children
+			cout << "\t\t\t" << char(186); Color(240); cout << "     (A Children Discount of 2% Deducted)"; Color(241); cout << setw(27) << char(186) << endl;
+			x++;
+		}
+		if (senior[index3] == 1) { //prints if passenger is senior
+			cout << "\t\t\t" << char(186); Color(240); cout << "     (A Seniors Discount of 20% Deducted)"; Color(241); cout << setw(27) << char(186) << endl;
+			x++;
+		}
+		if (group == 1) { //prints if passenger reservations is more than 5
+			cout << "\t\t\t" << char(186); Color(240); cout << "     (A Group Reservation Discount of 5% Deducted)"; Color(241); cout << setw(18) << char(186) << endl;
+			x++;
+		}
+		if (insurance1[0] == 'y' || insurance1[0] == 'Y') { //prints if passenger availed insurance
+			cout << "\t\t\t" << char(186); Color(240); cout << "     Insurance Fee"; Color(241); cout << setw(50) << char(186) << endl;
+			gotoxy(79, x);
+			Color(240); cout << "P" << insurance[index1]; Color(241); cout << endl;
+			x++;
+		}
+		if (BaggageFee[index1] != 0) { //prints if passenger has excess baggage
+			cout << "\t\t\t" << char(186); Color(240); cout << "     Excess Baggage Fee"; Color(241); cout << setw(45) << char(186) << endl;
+			gotoxy(79, x);
+			Color(240); cout << "P" << BaggageFee[index1]; Color(241); cout << endl;
+			x++;
+		}
+		cout << "\t\t\t" << char(186); Color(240); cout << " Total Passenger's Fee"; Color(241); cout << setw(46) << char(186) << endl; //prints out amount of each total passengers fee
+		gotoxy(79, x);
+		Color(240); cout << "P" << pssngr_fee[index2]; Color(241); cout << endl;
+		x += 2;
+		++ctr1;
+		++index1;
+		++index2;
+		++index3;
+	}
+	*/
+	//pause program so user can review or check the booking summary
+	cout << "\n\n\t\t\t ";
 	any_keypress();
 
 	// Output: Book Again
 	cout << logo;
-	// Asks again if user wants to book again
 	cout << "\n\n\n\t\t\t\t" << char(201);      for (int i = 0; i < 51; i++) { cout << char(205); }      cout << char(187) << endl;
 	cout << "\t\t\t\t" << char(186); Color(240); cout << "                                                   "; Color(241); cout << char(186) << endl;
-	cout << "\t\t\t\t" << char(186); Color(240); cout << "          Thank You for Booking with Merc!         "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t" << char(186); Color(240); cout << "        Thank You for Booking with JIJU!           "; Color(241); cout << char(186) << endl;
 	cout << "\t\t\t\t" << char(186); Color(240); cout << "                                                   "; Color(241); cout << char(186) << endl;
-	cout << "\t\t\t\t" << char(186); Color(240); cout << "           Would You Like to Book Again?           "; Color(241); cout << char(186) << endl;
-	cout << "\t\t\t\t" << char(186) << "                   [Y]/[N]:                        " << char(186) << endl;
-	cout << "\t\t\t\t" << char(186) << "                                                   " << char(186) << endl;
+	cout << "\t\t\t\t" << char(186); Color(240); cout << "          Would You Like to Book Again?            "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t" << char(186); Color(240); cout << "                  YES       NO                     "; Color(241); cout << char(186) << endl;
+	cout << "\t\t\t\t" << char(186); Color(240); cout << "                                                   "; Color(241); cout << char(186) << endl;
 	cout << "\t\t\t\t" << char(200);      for (int i = 0; i < 51; i++) { cout << char(205); }      cout << char(188) << endl;
-	gotoxy(61, 14);
-	gotoxy(1, 14);
-	ShowConsoleCursor(false);
-	cout << "\t\t\t\t"; Color(241); cout << char(186) << "                 " << ">YES"; Color(240); cout << "       NO                     "; Color(241); cout << char(186) << endl;
-	yn = 'Y';
-	gotoxy(54, 14);
-	running = true;
-	system("pause>nul");
-	while (running) {
+	arrow_keys();
 
-		if (GetAsyncKeyState(VK_RIGHT)) {
-			gotoxy(1, 14);
-			cout << "\t\t\t\t" << char(186); Color(240); cout << "                  YES"; Color(241); cout << "      >NO" << "                     " << char(186) << endl;
-			yn = 'N';
-			gotoxy(63, 14);
-			running = true;
-		}
-		if (GetAsyncKeyState(VK_LEFT)) {
-			gotoxy(1, 14);
-			cout << "\t\t\t\t"; Color(241); cout << char(186) << "                 " << ">YES"; Color(240); cout << "       NO                     "; Color(241); cout << char(186) << endl;
-			yn = 'Y';
-			gotoxy(54, 14);
-			running = true;
-		}
-		if (GetAsyncKeyState(VK_RETURN)) {
-			switch (yn) {
-			case 'y': case 'Y': //if input is YES run the program again
-				system("cls");
-				running = false;
-				main();
-				break;
-			case 'n': case 'N': // if NO stop the program
-				system("cls");
-				cout << logo;
-				cout << "\n\n\n\t\t\t\t\t      Thank You! And Goodbye!";
-				return 0;
-			}
-		}
-	}
 }
